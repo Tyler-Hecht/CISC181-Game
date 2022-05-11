a945dc62a9b8bda97c9a116cfdf1c59a15e445bd
Tyler Hecht <50639776+Tyler-Hecht@users.noreply.github.com>
2022-05-11 14:20:45 -0400

    Added paths for PieceBlueHen

--- PieceBlueHen.java
+++ PieceBlueHen.java
@@ -34,17 +34,34 @@ public class PieceBlueHen extends Piece implements Attacker, Recruiter {
     }
 
     public boolean validSpawnPath(int fromSquareRow, int fromSquareCol, int toSquareRow, int toSquareCol) {
-        return true;
+        if (this.canFly()) {
+            return true;
+        }
+        //diagonal
+        return Math.abs(fromSquareRow-toSquareRow)==1 && Math.abs(fromSquareCol-toSquareCol)==1;
     }
     public boolean validAttackPath(int fromSquareRow, int fromSquareCol, int toSquareRow, int toSquareCol) {
-        return true;
+        if (this.canFly()) {
+            return true;
+        }
+        //left or right
+        return (fromSquareRow==toSquareRow) && Math.abs(fromSquareCol-toSquareCol)==1;
     }
     public boolean validRecruitPath(int fromSquareRow, int fromSquareCol, int toSquareRow, int toSquareCol) {
-        return true;
+        if (this.canFly()) {
+            return true;
+        }
+        //up or down
+        return (fromSquareCol==toSquareCol) && Math.abs(fromSquareRow-toSquareRow)==1;
     }
     @Override
     public boolean validMovePath(int fromSquareRow, int fromSquareCol, int toSquareRow, int toSquareCol) {
-        return true;
+        if (this.canFly()) {
+            return true;
+        }
+        //any of the 8 nearest squares
+        //since the distance will only be 0, 1, sqrt2, sqrt3, etc, the +0.01 should handle rounding errors
+        return Math.sqrt(Math.pow(fromSquareRow-toSquareRow,2)+Math.pow(fromSquareCol-toSquareCol,2)) <= Math.sqrt(2) + 0.01;
     }
 
     public void setNumRecruits(int numRecruits) {
@@ -53,20 +70,14 @@ public class PieceBlueHen extends Piece implements Attacker, Recruiter {
 
 
     private void updateFly()    {
-        if (this.numAttacks < MAX_NUM_ATTACKS){
-            this.flies = true;
-        }
-        else {
-            this.flies = false;
-        }
+        this.flies = (this.numAttacks < MAX_NUM_ATTACKS);
     }
     public void speak(){
         System.out.println("Go UD!");
     }
 
     public PieceBlueHen spawn() {
-        PieceBlueHen copyHen = new PieceBlueHen(Character.toLowerCase(this.symbol), this.teamColor,this.numAttacks,this.numRecruits, false,false);
-        return copyHen;
+        return new PieceBlueHen(Character.toLowerCase(this.symbol), this.teamColor,this.numAttacks,this.numRecruits, false,false);
     }
     public boolean canSpawn(){
         return true;
